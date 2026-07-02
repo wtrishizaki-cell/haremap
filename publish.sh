0243f85d5bb24457262c5e918b6641f60eba40ec
wtrishizaki-cell <wtrishizaki@gmail.com>
2026-07-03 02:38:02 +0900

    自動更新チェック追加（スマホ復帰時に最新版を自動反映）

--- publish.sh
+++ publish.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cd "$(dirname "$0")"
+BUILD=$(date +%s)
+sed -i '' "s/name=\"build-time\" content=\"[^\"]*\"/name=\"build-time\" content=\"${BUILD}\"/" index.html
 git add index.html
 git commit -m "ハレマップ更新 $(date '+%Y/%m/%d %H:%M')"
 git push
